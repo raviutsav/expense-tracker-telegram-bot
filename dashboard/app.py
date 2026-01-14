98d31604fb15d892eb64b82fcc809af6116b75e6
Ravi <raviutsav0@gmail.com>
2026-01-14 16:36:47 +0530

    updated flask to take request from anywhere

--- dashboard/app.py
+++ dashboard/app.py
@@ -322,4 +322,4 @@ def submit_feature_request():
 # Run the app
 # ----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
